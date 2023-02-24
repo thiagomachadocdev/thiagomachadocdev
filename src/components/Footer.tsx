ce8a7858cbf8073b41dbb3ad83b1d22839f3f83c
Thiago Machado <contatocostathiago@gmail.com>
2023-02-24 15:37:55 -0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,14 +2,6 @@ function Footer() {
   return (
     <div className="fixed bottom-0 py-[25px] px-[40px] gap-[16px] w-full flex flex-col md:flex-row items-end md:items-center justify-center">
       <a
-        href="https://www.instagram.com/thiagomachadocdev/"
-        target="_blank"
-        className="text-gray-100"
-      >
-        Instagram
-      </a>
-
-      <a
         href="https://github.com/thiagomachadocdev/"
         target="_blank"
         className="text-gray-100"
